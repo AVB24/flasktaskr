340b4a7192b5abf843464d2b3a7169dddf424d32
AVB24 <GsnShadow24@gmail.com>
2018-02-12 14:58:50 -0500

    SQLAlchemy
    SQL Alchemy added and switched to using that for DB creation and views

--- project/_config.py
+++ project/_config.py
@@ -6,8 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 SECRET_KEY = 'b"\x80Y\x89\xa6zk\xc3!\xe7Ve\xa5v\xbf\xcc\x96\xee\xf4\x9aZ"\xe7\xb1\xaa"'
 
 # define the full path for the DATABASE
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
