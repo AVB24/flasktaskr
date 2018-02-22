3e7ba7282e393774546d4959ef023634e0984360
AVBMac <gsnshadow24@gmail.com>
2018-02-22 14:57:34 -0500

    make it better

---
+++ db_create.py
@@ -0,0 +1,10 @@
+# db_create.py
+#from datetime import date
+from project import db
+#from project.models import Task, User
+# create the database and the db table
+db.create_all()
+# insert data
+
+# commit the changes
+db.session.commit()
